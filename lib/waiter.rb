0a02a07150d59df861a2842c1d8e00f58b25ec30
Daniel Reina <dannyflatiron@gmail.com>
2019-12-19 04:08:30 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,7 +1,15 @@
 class Waiter
 
+
+@@all = []
+
   def initialize(name , years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
+    @@all << self
+  end
+
+  def self.all?
+    @@all
   end
 end
