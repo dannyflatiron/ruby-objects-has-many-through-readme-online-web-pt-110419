fa16d2938a6a481a82f59ded14021be943af7ee3
Daniel Reina <dannyflatiron@gmail.com>
2019-12-19 05:06:19 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -23,4 +23,10 @@ attr_accessor :name, :years_of_experience
       meal.waiter == self
     end
   end
+
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+  end
 end
